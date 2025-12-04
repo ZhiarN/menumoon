e1a41d72ab311af8975d9f33f3dd19fdf1ecaa07
ZhiarN <tetrodot@protonmail.com>
2025-12-05 00:50:33 +0330

    add menu items and loading screen

---
+++ script.js
@@ -0,0 +1,11 @@
+const body = document.querySelector("body");
+
+// document.onreadystatechange = function () {
+//   if (document.readyState !== "complete") {
+//     document.querySelector("body").style.visibility = "hidden";
+//     document.querySelector("#loading").style.visibility = "visible";
+//   } else {
+//     document.querySelector("#loading").style.display = "none";
+//     document.querySelector("body").style.visibility = "visible";
+//   }
+// };
