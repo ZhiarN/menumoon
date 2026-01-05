4c32fa97310a17abcfc428060874c23ff530d8f2
ZhiarN <tetrodot@protonmail>
2026-01-05 05:20:47 +0330

    Add function to return category ID on selection. Minor css tweaks.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const body = document.querySelector("body");
-
+let categories = document.querySelector(".categories");
+let categoryID;
+const menu = document.querySelector(".menu");
+let target;
 // document.onreadystatechange = function () {
 //   if (document.readyState !== "complete") {
 //     document.querySelector("body").style.visibility = "hidden";
@@ -9,3 +12,41 @@ const body = document.querySelector("body");
 //     document.querySelector("body").style.visibility = "visible";
 //   }
 // };
+categories.addEventListener("click", (event) => {
+target = event.target.closest("div");
+if (!target.classList.contains("categoryItemCard")) return;
+    selectCategory(target);
+})
+
+    function selectCategory(target) {
+        const isSelected = target.classList.contains("selected");
+        const selectedCategory = document.querySelector(".selected");
+        const isCategory = target.classList.contains("categoryItemCard");
+        categoryID = target.id;
+        console.log("ID: " + categoryID)
+        if (!isCategory) return;
+        if (isSelected) {
+            target.classList.remove("selected");
+        }
+
+        else {
+            if (selectedCategory) {
+                selectedCategory.classList.remove("selected")
+            }
+            target.classList.add("selected");
+            
+            
+           
+
+        }
+    }
+    // if 
+    // let item = {
+    //     uID: uniqueItemID,
+    //     name: itemName,
+    //     img: itemIMG,
+    //     price: itemPrice,
+    //     allergens: itemAllergens,
+    //     category: itemCategory,
+
+    // }
