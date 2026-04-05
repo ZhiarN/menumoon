ce429b1b7b2f17a9f6087c069639da91a5fd5e59
ZhiarN <zhiarnb@gmail.com>
2026-04-05 22:55:28 +0330

    Add dom.js and state.js modules

---
+++ src/js/dom.js
@@ -0,0 +1,9 @@
+export const appRoot = document.querySelector(`[data-component="root"]`);
+export const editButton = document.querySelector(`[data-action="toggle-edit"]`);
+export const menuElement = document.querySelector(
+  `[data-component="menu-items"]`,
+);
+export const categoryListElement = document.querySelector(
+  `[data-component="category-list"]`,
+);
+export const categoryNavElement = document.querySelector(".category-nav");
