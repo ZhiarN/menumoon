980ce7141c8953d70d2a14fe7a6d4664d72c3059
ZhiarN <zhiarnb@gmail.com>
2026-04-05 23:47:12 +0330

    Add itemActions.js module

---
+++ src/js/actions/itemActions.js
@@ -0,0 +1,35 @@
+import { state } from "../state.js";
+import { renderUI } from "../render/renderUI.js";
+export function removeItem() {
+  if (state.selectedItemID == null) return alert("No Item Selected");
+  const category = state.categories.find(
+    (cat) => cat.id === state.selectedCategoryID,
+  );
+  const index = category.items.findIndex(
+    (item) => item.id === state.selectedItemID,
+  );
+  if (index === -1) return;
+  category.items.splice(index, 1);
+  state.selectedItemID = null;
+  renderUI();
+}
+
+export function addItem(newItemName, newItemPrice, newItemImage) {
+  if (state.selectedCategoryID == null) {
+    alert("No Category Selected");
+    return;
+  }
+  const category = state.categories.find(
+    (cat) => cat.id === state.selectedCategoryID,
+  );
+
+  const newItem = {
+    id: `${category.id}_item${category.items.length + 1}`,
+    name: newItemName,
+    price: newItemPrice,
+    image: newItemImage,
+  };
+
+  category.items.push(newItem);
+  renderUI();
+}
