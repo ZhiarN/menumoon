14a8ff683df9a5ed367517ee6c06d047818b7fed
ZhiarN <zhiarnb@gmail.com>
2026-04-06 00:12:37 +0330

    Remove dom queries and add renderUI on item event

---
+++ src/js/events/addListeners.js
@@ -0,0 +1,28 @@
+import { state } from "../state.js";
+import { renderUI } from "../render/renderUI.js";
+import { dom } from "../dom.js";
+export function addListeners() {
+  dom.categoryListElement.addEventListener("click", (event) => {
+    const card = event.target.closest("[data-action=select-category]");
+    if (!card) return;
+    if (card.dataset.categoryId === state.selectedCategoryID) {
+      state.selectedCategoryID = null;
+      state.selectedItemID = null;
+      return;
+    }
+    state.selectedCategoryID = card.dataset.categoryId;
+    state.selectedItemID = null;
+    renderUI();
+  });
+  dom.menuElement.addEventListener("click", (event) => {
+    const item = event.target.closest("[data-action=select-item]");
+    if (!item) return;
+    if (state.selectedItemID === item.dataset.itemId) {
+      state.selectedItemID = null;
+    } else {
+      state.selectedCategoryID = item.dataset.categoryId;
+      state.selectedItemID = item.dataset.itemId;
+    }
+    renderUI();
+  });
+}
