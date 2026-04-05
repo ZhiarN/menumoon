910f72511c5a3fd300876decc01045a3e99e7c47
ZhiarN <zhiarnb@gmail.com>
2026-04-06 00:11:20 +0330

    Add DOM object to hold DOM queries

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,9 +1,9 @@
-export const appRoot = document.querySelector(`[data-component="root"]`);
-export const editButton = document.querySelector(`[data-action="toggle-edit"]`);
-export const menuElement = document.querySelector(
-  `[data-component="menu-items"]`,
-);
-export const categoryListElement = document.querySelector(
-  `[data-component="category-list"]`,
-);
-export const categoryNavElement = document.querySelector(".category-nav");
+export const dom = {
+  appRoot: document.querySelector(`[data-component="root"]`),
+  editButton: document.querySelector(`[data-action="toggle-edit"]`),
+  menuElement: document.querySelector(`[data-component="menu-items"]`),
+  categoryListElement: document.querySelector(
+    `[data-component="category-list"]`,
+  ),
+  categoryNavElement: document.querySelector(".category-nav"),
+};
