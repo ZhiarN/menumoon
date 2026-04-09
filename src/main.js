9819ced787c1cc7429fe9b2bd17c167490d97911
ZhiarN <zhiarnb@gmail.com>
2026-04-10 02:01:42 +0330

    Add helper to validate dom elements at startup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { renderUI } from "./render/renderUI.js";
 import { addListeners } from "./events/addListeners.js";
-
+import { validateDOM } from "./utils/domValidate.js";
+validateDOM();
 function startApp() {
   addListeners();
   renderUI();
