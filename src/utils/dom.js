e1c5e50358fbeb0002283c5a8e8dbcc2abf56cd7
ZhiarN <zhiarnb@gmail.com>
2026-04-12 15:45:21 +0330

    Add empty fallback string for missing elements

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,11 +1,10 @@
 export const dom = {
-  appRoot: document.querySelector(`[data-component="root"]`),
-  editButton: document.querySelector(`[data-action="toggle-edit"]`),
-  menuElement: document.querySelector(`[data-component="menu-items"]`),
-  categoryListElement: document.querySelector(
-    `[data-component="category-list"]`,
-  ),
-  categoryNavElement: document.querySelector(".category-nav"),
-  itemTemplate: document.getElementById("item-card-template"),
-  categoryTemplate: document.getElementById("category-card-template"),
+  appRoot: document.querySelector(`[data-component="root"]`) || "",
+  editButton: document.querySelector(`[data-action="toggle-edit"]`) || "",
+  menuElement: document.querySelector(`[data-component="menu-items"]`) || "",
+  categoryListElement:
+    document.querySelector(`[data-component="category-list"]`) || "",
+  categoryNavElement: document.querySelector(".category-nav") || "",
+  itemTemplate: document.getElementById("item-card-template") || "",
+  categoryTemplate: document.getElementById("category-card-template") || "",
 };
